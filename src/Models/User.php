6dac17aa10cabbef94c516fc0e3041ff4484cc5c
Lee Hilton <lee.hilton@leezilla.net>
2019-11-01 11:08:24 -0700

    Additional larastan updates

--- src/Models/User.php
+++ src/Models/User.php
@@ -4,6 +4,34 @@ namespace Riverbedlab\Industrialist\Models;
 
 use Riverbedlab\Industrialist\Models\AbstractStatelessModel as Model;
 
+/**
+ * A model of the expected user object returned by Driver implmentations
+ *
+ * @method ?string getNameId()
+ * @method ?string setNameId(?string $value)
+ * @method ?string getNameIdFormat()
+ * @method ?string setNameIdFormat(?string $value)
+ * @method bool getIsAuthenticated()
+ * @method bool setIsAuthenticated(bool $value)
+ * @method ?string getSessionIndex()
+ * @method ?string setSessionIndex(?string $value)
+ * @method ?string getSessionExpiration()
+ * @method ?int setSessionExpiration(?int $value)
+ * @method ?array getErrors()
+ * @method ?array setErrors(?array $value)
+ * @method ?string getLastRequestId()
+ * @method ?string setLastRequestId(?string $value)
+ * @method ?string getErrorReason()
+ * @method ?string setErrorReason(?string $value)
+ * @method ?string getLastResponse()
+ * @method ?string setLastResponse(?string $value)
+ * @method ?string getLastRequest()
+ * @method ?string setLastRequest(?string $value)
+ * @method ?array getAttributes()
+ * @method ?arrat setAttributes(?array $value)
+ * @method ?array setAttributesWithFriendlyName()
+ * @method ?array setAttributesWithFriendlyName(?array $value)
+ */
 class User extends Model
 {
     protected $attributes = [
